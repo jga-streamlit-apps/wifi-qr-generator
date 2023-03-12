06c0bbca6217f12cc1d74683334d7e3d1adcb255
jga <jga@evaxion-biotech.com>
2023-03-12 11:39:09 +0100

    cached qr generation

--- wifi_qr_generator/ui.py
+++ wifi_qr_generator/ui.py
@@ -50,7 +50,17 @@ def preview(image: io.BytesIO) -> None:
     st.image(preview_image, use_column_width=False)
 
 
-def createQRcode(
+@st.cache
+def generate_qr_code(ssid, password, security, hidden, kind, scale):
+    out = io.BytesIO()
+    qrcode = helpers.make_wifi(
+        ssid, password=password, security=security, hidden=hidden
+    )
+    qrcode.save(out, kind=kind, dark="#00000b", light=None, scale=scale)
+    return out
+
+
+def create_and_preview_qr(
     ssid: str,
     password: str | None = None,
     security: str | None = None,
@@ -65,13 +75,23 @@ def createQRcode(
         step=1,
         help="Scales the produced image",
     )
-    out = io.BytesIO()
-    qrcode = helpers.make_wifi(
-        ssid, password=password, security=security, hidden=hidden
-    )
-    qrcode.save(out, kind=kind, dark="#00000b", light=None, scale=scale)
+    out = generate_qr_code(ssid, password, security, hidden, kind, scale)
     preview(out)
     return out
+
+
+def qr_explanation():
+    return st.markdown(
+        """A QR code (short for Quick Response code) is a two-dimensional barcode that can be scanned with a smartphone or other device to access information. In the context of logging into a WiFi network, a QR code can be used to quickly and easily connect to the WiFi network without having to manually enter the network name (SSID) and password!
+
+    Here's how it works:
+
+    1. Generate a QR code with the SSID (WiFi name) and password.
+    2. Anyone  who wants to connect to the WiFi network scans the QR code with a smartphone or other device equipped with a QR code reader.
+    3. The QR code reader decodes the QR code and automatically connects the user's device to the WiFi
+
+    This process eliminates the need for the user to manually enter the SSID and password, making it easier and faster to connect to the WiFi network. It also helps to ensure that the correct SSID and password are used without the hassle of typos. This allows you to have a strong password without the hassle of having to type it."""
+    )
 
 
 def main():
@@ -85,20 +105,10 @@ def main():
     ssid, password, security, hidden = inputForm()
 
     st.subheader("QR Code")
-    out = createQRcode(ssid, password, security, hidden)
+    out = create_and_preview_qr(ssid, password, security, hidden)
 
     st.subheader("About")
-    st.markdown(
-        """A QR code (short for Quick Response code) is a two-dimensional barcode that can be scanned with a smartphone or other device to access information. In the context of logging into a WiFi network, a QR code can be used to quickly and easily connect to the WiFi network without having to manually enter the network name (SSID) and password!
-
-Here's how it works:
-
-1. Generate a QR code with the SSID (WiFi name) and password.
-2. Anyone  who wants to connect to the WiFi network scans the QR code with a smartphone or other device equipped with a QR code reader.
-3. The QR code reader decodes the QR code and automatically connects the user's device to the WiFi
-
-This process eliminates the need for the user to manually enter the SSID and password, making it easier and faster to connect to the WiFi network. It also helps to ensure that the correct SSID and password are used without the hassle of typos. This allows you to have a strong password without the hassle of having to type it."""
-    )
+    qr_explanation()
 
 
 if __name__ == "__main__":
