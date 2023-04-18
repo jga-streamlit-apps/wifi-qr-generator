dacf70493c00733b1982467b302d89b626de0e9c
jga <jga@evaxion-biotech.com>
2023-04-18 20:10:55 +0200

    refactor(ui.py): remove unused download button and cache decorators feat(ui.py): add download button after generating QR code The download button was removed from the preview function and added to the create_and_preview_qr function. The preview function now only returns the image object. The st.cache decorator was removed from both preview and generate_qr_code functions as they were not necessary. A new download button was added after generating the QR code. This allows the user to download the QR code image after it has been generated.

--- wifi_qr_generator/ui.py
+++ wifi_qr_generator/ui.py
@@ -36,21 +36,15 @@ def inputForm() -> list[str, str | None, str | None, bool]:
     return ssid, password, security, hidden
 
 
+@st.cache(suppress_st_warning=True)
 def preview(image: io.BytesIO) -> None:
     preview_image = image
     image = Image.open(image)
     width, height = image.size
-    st.download_button(
-        "Download QR code",
-        preview_image,
-        file_name="wifi-qr-code.png",
-        mime="image/png",
-    )
     st.markdown(f"Image size: {width}x{height}")
     st.image(preview_image, use_column_width=False)
 
 
-@st.cache
 def generate_qr_code(ssid, password, security, hidden, kind, scale):
     out = io.BytesIO()
     qrcode = helpers.make_wifi(
@@ -66,9 +60,9 @@ def create_and_preview_qr(
     security: str | None = None,
     hidden: bool = False,
     kind: str = "png",
-) -> io.BytesIO:
+) -> None:
     scale = st.slider(
-        "Image scale (expand image to view full size)",
+        "Scales image `x` times (expand image to view full size)",
         min_value=1,
         max_value=100,
         value=5,
@@ -76,39 +70,46 @@ def create_and_preview_qr(
         help="Scales the produced image",
     )
     out = generate_qr_code(ssid, password, security, hidden, kind, scale)
-    preview(out)
-    return out
+    image = preview(out)
+    st.download_button(
+        "Download QR code",
+        out,
+        file_name="wifi-qr-code.png",
+        mime="image/png",
+    )
 
 
 def qr_explanation():
     return st.markdown(
         """A QR code (short for Quick Response code) is a two-dimensional barcode that can be scanned with a smartphone or other device to access information. In the context of logging into a WiFi network, a QR code can be used to quickly and easily connect to the WiFi network without having to manually enter the network name (SSID) and password!
 
-    Here's how it works:
+Here's how it works:
 
-    1. Generate a QR code with the SSID (WiFi name) and password.
-    2. Anyone  who wants to connect to the WiFi network scans the QR code with a smartphone or other device equipped with a QR code reader.
-    3. The QR code reader decodes the QR code and automatically connects the user's device to the WiFi
+1. Generate a QR code with the SSID (WiFi name) and password.
+2. Anyone  who wants to connect to the WiFi network scans the QR code with a smartphone or other device equipped with a QR code reader.
+3. The QR code reader decodes the QR code and automatically connects the user's device to the WiFi
 
-    This process eliminates the need for the user to manually enter the SSID and password, making it easier and faster to connect to the WiFi network. It also helps to ensure that the correct SSID and password are used without the hassle of typos. This allows you to have a strong password without the hassle of having to type it."""
+This process eliminates the need for the user to manually enter the SSID and password, making it easier and faster to connect to the WiFi network. It also helps to ensure that the correct SSID and password are used without the hassle of typos. This allows you to have a strong password without the hassle of having to type it."""
     )
 
 
 def main():
     security = "WPA"
-    st.title("WiFi QR Code Generator")
+    st.subheader("Parameters")
     st.markdown(
         """Fill in the boxed below to create a QR code for easy login on the specified wifi"""
     )
 
-    st.subheader("Parameters")
     ssid, password, security, hidden = inputForm()
 
     st.subheader("QR Code")
-    out = create_and_preview_qr(ssid, password, security, hidden)
+    create_and_preview_qr(ssid, password, security, hidden)
 
     st.subheader("About")
-    qr_explanation()
+
+    with st.sidebar:
+        st.title("WiFi QR Code Generator")
+        qr_explanation()
 
 
 if __name__ == "__main__":
